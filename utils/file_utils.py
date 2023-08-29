9542ffac86f81046d704a56bc8bb883ff74bb567
tunv <tunguyen060811@gmail.com>
2023-08-29 19:59:03 +0700

    feat: add hint

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,7 +1,7 @@
 import os
 
 
-def check_directory_exist(dir_path):
+def check_directory_exist(dir_path: str) -> None:
     """Create directory if not exist."""
     if not os.path.exists(dir_path):
         os.makedirs(dir_path, exist_ok=True)
